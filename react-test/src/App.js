5c36485f1147b12b71cbad399f1710bc0f8628c5
Wu <WuRe@analytics.moodys.net>
2018-07-11 11:02:22 +0800

    test

---
+++ react-test/src/App.js
@@ -0,0 +1,49 @@
+// import React, { Component } from 'react';
+// import ReactDOM from 'react-dom';
+// import './App.css';
+
+// var a = 5;
+// var b = 10;
+// var result = '15 is ${a+b} not ${a}';
+// console.log('result:' + result);
+// console.log(`result: 15 is ${a + b} not ${a}`);
+// var materials = [
+//   'plastic',
+//   'metal'
+// ];
+// var m1 = materials.map(m => m.length);
+// var m2 = materials.map(m => {
+//   m += 'aa'
+//   return m.length;
+// });
+// console.log(m1 + ';' + m2);
+
+
+// var sum = 98;
+// var result1 = (sum < 60) ? 'failed' : (sum < 90) ? 'good' : 'excellent';
+// console.log(result1)
+
+// function formateName(user){
+//   return user.firstName + ' ' + user.lastName;
+// }
+// const user = {
+//   firstName: 'Rebecca',
+//   lastName: 'Wu'
+// };
+
+// function tick(){
+//   const element = (
+//     <div>
+//       <h1>Hello {formateName(user)}</h1>
+//       <h2>result {a}</h2>
+//       <h3>It is {new Date().toLocaleTimeString()}</h3>
+//     </div>
+//   );
+
+//   ReactDOM.render(
+//     element,
+//     document.getElementById('rsoot')
+//   );
+// }
+// tick()
+// setInterval(tick, 1000);
